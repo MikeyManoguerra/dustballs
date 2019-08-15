fceef766d207c933e857afa46688a18f4d161020
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-08-15 09:58:32 -0400

    making progress splitting scenes. individual lines have their own objects, handling stage direction edge cases. only one scene at a time, no larger loop

--- Source_Material/parsing_scripts/alls_well_parser.py
+++ Source_Material/parsing_scripts/alls_well_parser.py
@@ -9,12 +9,42 @@ act_dicts = []
 intro_prep = re.split(
     r'(?<=Tuscany\.)\n', play['ALL\u2019S WELL THAT ENDS WELL'], maxsplit=1)
 intro = intro_prep[0]
-dialogue = intro_prep[1]
+work_body = intro_prep[1]
 
-acts = re.split(r'\n+(?=ACT)' , dialogue)
+acts = re.split(r'\n+(?=ACT)', work_body)
 
 # for act in acts:
-scenes = re.split(r'\n+(?=SCENE)', acts[3])
+scenes = re.split(r'\n+(?=SCENE)', acts[5])
+lines = re.split(
+    r'\n+(?=[A-Z\s]+\.)|\n+\s{2,}(?=\[_)|\n+\s+(?=Enter)', scenes[2])
 
-pp = pprint.PrettyPrinter(indent=3)
-pp.pprint(scenes[0])
+parsed_scene = []
+
+for line in lines:
+  player_or_direct = re.split(r'(?<=[A-Z]\.)\n+', line)
+
+  try:
+    text = {
+      'type': 'dialouge',
+      'character': player_or_direct[0],
+      'line': player_or_direct[1]
+    }
+  except IndexError:
+    try:
+      direct = re.split(r'(?<=_\])$|\n+', player_or_direct[0])
+      text = {
+        'type': 'direction',
+        'direction': direct[0],
+        'leftover': direct[1]
+
+      }
+    except IndexError:
+      text = direct
+    
+  parsed_scene.append(text)  
+
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(parsed_scene)
+
+# print(len(lines))
