cf09a3e2a092e25e68e2e4674fdb6d18069f4cad
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-09-27 08:48:33 -0400

    returned a random sonnet, keep app running.

--- server/index.py
+++ server/index.py
@@ -1,13 +1,21 @@
-# from flask import Flask
-# from pymongo import MongoClient
-# client = MongoClient('localhost', 27017)
-# db = client.dustball_db
+from flask import Flask
+from flask import jsonify
+from pymongo import MongoClient
+import random
+client = MongoClient('localhost', 27017)
+db = client.dustball_db
+sonnets = db.sonnets
+
+# http://flask.palletsprojects.com/en/1.1.x/tutorial/layout/
+app = Flask(__name__)
 
 
+@app.route('/')
+def hello_world():
+    random_number = str(random.randint(1, 155))
+    sonnet = sonnets.find_one({"title": random_number})
 
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-
+    # https://stackoverflow.com/questions/16586180/typeerror-objectid-is-not-json-serializable
+    return jsonify({"text":sonnet['text'],'title': sonnet['title']})
+if __name__ == "__main__":
+    app.run(debug=True)
